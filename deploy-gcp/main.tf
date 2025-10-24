267f5af45303a4b8fd550bf4f0466ba4aaa7ba59
Ruben Flam-Shepherd <ruben.flam.shepherd@gmail.com>
2025-10-24 22:10:05 +0000

    Allow build SA to manage artifact repo IAM

--- deploy-gcp/main.tf
+++ deploy-gcp/main.tf
@@ -198,6 +198,16 @@ resource "google_artifact_registry_repository_iam_member" "cloud_run_runtime_acc
   ]
 }
 
+resource "google_artifact_registry_repository_iam_member" "build_service_account_repo_access" {
+  project    = var.project_id
+  location   = google_artifact_registry_repository.services.location
+  repository = google_artifact_registry_repository.services.repository_id
+  role       = "roles/artifactregistry.repoAdmin"
+  member     = "serviceAccount:${google_service_account.build.email}"
+
+  depends_on = [google_project_service.required["artifactregistry.googleapis.com"]]
+}
+
 resource "google_cloud_run_service" "app" {
   name     = var.app_name
   location = local.region
