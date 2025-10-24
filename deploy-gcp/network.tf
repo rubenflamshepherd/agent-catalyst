61cbddcd84087f577b8a088aba9d7f36f9de7c96
Ruben Flam-Shepherd <ruben.flam.shepherd@gmail.com>
2025-10-24 23:08:21 +0000

    remove placeholder network

--- deploy-gcp/network.tf
+++
@@ -1,6 +0,0 @@
-// ABOUTME: Creates networking resources for the infrastructure.
-// ABOUTME: Provisions VPC network for resource connectivity.
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
